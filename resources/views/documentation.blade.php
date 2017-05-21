8c11566ebdc358154644898625680734673ca35f
Dilip Pareba <dilippareba@gmail.com>
2017-05-21 08:54:44 +0530

    update design of welcome.blade.php

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -4,6 +4,12 @@
 
 	^ Add Authentication Scaffolding to project using php artisan make:auth
 
-	
+	^ In config/app.php, change value of 'name' => env('APP_NAME', 'ClinicJet'),
+
+	^ .env file, update APP_NAME=ClinicJet
+
+	^ Update the design of welcome.blade.php
+
+	^ update route of Home to @@<a class="link-doc" href="{{ url('check') }}">Home</a>@@ in welcome.blade.php 
 
 --}}
